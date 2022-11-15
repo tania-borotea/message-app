4d193a53cb4c0d7f438057d7cc8143787704e1d2
tania-borotea <tania.borotea@reea.net>
2022-11-15 15:05:54 +0200

    add navigation

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+
+//= require rails-ujs
+//= require jquery
+//= require activestorage
+//= require turbolinks
+//= require semantic-ui
+//= require_tree .
+
+$(document).on("turbolinks:load", function () {
+	$(".ui.dropdown").dropdown();
+});
